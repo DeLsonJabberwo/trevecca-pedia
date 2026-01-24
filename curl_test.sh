63a57a21d17b77d2ce179805a358555bc85db643
DeLsonJabberwo <pdhl2006@gmail.com>
2026-01-24 16:24:46 -0600

    removed testing files

--- curl_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -X POST 127.0.0.1:9454/pages/new \
--F "slug=spiritual-life" \
--F "name=Spiritual Life" \
--F "author=1197028" \
--F "archive_date=" \
--F "new_page=@new_page.md"
-
